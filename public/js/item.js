7cd3a7f554bc774000d114164bd7a2b90c4314b0
Caleb <cgomez150@gmail.com>
2016-12-11 11:36:54 -0500

    update item

--- public/js/item.js
+++ public/js/item.js
@@ -6,9 +6,14 @@
     var confirmUpdateWrapper = $("#confirmUpdateWrapper");
     var cancelUpdate = $("#cancelUpdateButton");
     var confirmUpdate = $("#confirmUpdateButton");
-    var cancelDelete = $("#cancelDelete");
-    var confirmDelete = $("#confirmDelete");
-    var itemId = $("#itemId").val();
+    var cancelDelete = $("#cancelDeleteButton");
+    var confirmDelete = $("#confirmDeleteButton");
+    var itemId = $("#itemId").text();
+    var itemEdit = $("#itemEdit");
+    var itemUpdate = $("#itemUpdateButton");
+    var errorContainer = $("#errorContainer");
+    var errorText = $("#errorText");
+
 
     // var nameEditButton = $("#nameEditButton");
     var nameInput = $("#nameInput");
@@ -43,11 +48,12 @@
     cancelDelete.click(function(){
         confirmDeleteWrapper.addClass("hidden");
     });
+    
     confirmDelete.click(function(){
         //Make request to delete user
         var requestConfig = {
             method: "Delete",
-            url: "/items/delete/"+itemId,
+            url: "/items/"+itemId,
             contentType: 'application/json'
         };
         $.ajax(requestConfig).then(function (responseMessage){
@@ -59,42 +65,69 @@
     });
 
     itemEdit.click(function(){
-        try{
+        nameInput.removeClass("hidden");
+        quantityInput.removeClass("hidden");
+        categoryInput.removeClass("hidden");
+        priceInput.removeClass("hidden");
+        descriptionInput.removeClass("hidden");
+        itemUpdate.removeClass("hidden");
+    });
 
-        
-            nameInput.removeClass("hidden");
-            quantityInput.removeClass("hidden");
-            categoryInput.removeClass("hidden");
-            priceInput.removeClass("hidden");
-            descriptionInput.removeClass("hidden");
-            nameUpdateButton.removeClass("hidden");
+    itemUpdate.click(function(){
+        confirmUpdateWrapper.removeClass("hidden");
+    });
 
+    cancelUpdate.click(function(){
+        confirmUpdateWrapper.addClass("hidden");
+    })
+
+    confirmUpdate.click(function(){
+        try{      
+            console.log("hrey");
             var itemName = $("#nameInput");
             var category = $("#categoryInput");
             var description = $("#descriptionInput");
             var quantity = $("#quantityInput");
             var price = $("#priceInput");
 
+
+
             var itemNameVal = itemName.val();
             var categoryVal = category.val();
             var descriptionVal = description.val();
             var quantityVal = quantity.val();
             var priceVal = price.val();
+            
+            var propsToUpdate = {};
+
+            if(itemNameVal != ""){
+                propsToUpdate.itemName = itemNameVal;
+            }
+
+            if(categoryVal != ""){
+                propsToUpdate.category = categoryVal;
+            }
+
+            if(descriptionVal != ""){
+                propsToUpdate.description = descriptionVal;
+            }
+
+            if(quantityVal){
+                propsToUpdate.itemQuantity = quantityVal;
+            }
+
+            if(price){
+                propsToUpdate.price = priceVal;
+            }
 
             errorContainer.addClass("hidden");
 
-            // var requestConfig = {
-            //     method: "Post",
-            //     url: "/items/add/",
-            //     data: JSON.stringify({
-            //         "itemName":itemNameVal,
-            //         "description": descriptionVal,
-            //         "itemQuantity": quantityVal,
-            //         "price": priceVal,
-            //         "category": categoryVal
-            //     }),
-            //     contentType: 'application/json'
-            // };
+            var requestConfig = {
+                method: "Put",
+                url: "/items/update/"+itemId,
+                data: JSON.stringify(propsToUpdate),
+                contentType: 'application/json'
+            };
 
             $.ajax(requestConfig).then(function (responseMessage){
                 //If we got a response populate the page
@@ -103,18 +136,23 @@
                         errorContainer.removeClass("hidden");
                         errorText.text(responseMessage.error);
                     }
-                    $("#itemTable tr:last").after('<tr class="itemRow"><td>'+responseMessage.name+'</td><td>'
-                    +responseMessage.category+'</td><td>'
-                    +responseMessage.price+'</td><td>'
-                    +responseMessage.quantity+'</td></tr>');
+                    nameField.text(responseMessage.name);
+                    categoryField.text(responseMessage.category);
+                    descriptionField.text(responseMessage.description);
+                    quantityField.text(responseMessage.quantity);
+                    priceField.text(responseMessage.price);
                     
                 }
             });
 
+        }catch(e){
+            errorContainer.removeClass("hidden");
+            errorText.text(e.message);
         }
+
+        return false;
     });
 
     
-
 
 })();
