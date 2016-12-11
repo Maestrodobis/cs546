b97e4749db58e3b25a3d5a6d372b2c04f66f2c3c
Caleb <cgomez150@gmail.com>
2016-12-10 23:06:55 -0500

    inventory and item scripts

--- public/js/item.js
+++ public/js/item.js
@@ -1,5 +1,5 @@
 
-(function ($, item) {
+(function () {
 
     var itemForm = $("#updateItemForm");
     var itemNameInput = $("#itemName");
@@ -7,6 +7,34 @@
     var priceInput = $("#price");
     var quantityInput = $("#quantity");
     var descriptionInput = $("#descriptionInput");
+
+    var deleteItem = $("#deleteItem");
+    var confirmDeleteWrapper = $("#confirmDeleteWrapper");
+    var cancel = $("#cancelButton");
+    var confirm = $("#confirmButton");
+    var itemId = $("#itemId").val();
+    
+    deleteItem.click(function(){
+        confirmDeleteWrapper.removeClass("hidden");
+    });
+
+    cancel.click(function(){
+        confirmDeleteWrapper.addClass("hidden");
+    });
+    confirm.click(function(){
+        //Make request to delete user
+        var requestConfig = {
+            method: "Delete",
+            url: "/items/delete/"+itemId,
+            contentType: 'application/json'
+        };
+        $.ajax(requestConfig).then(function (responseMessage){
+            //If we got a response populate the page
+            if( responseMessage ){
+                window.location.href = "/items";
+            }
+        });
+    });
 
     itemForm.submit(function(event) {
         event.preventDefault();
@@ -17,9 +45,10 @@
         var quantity = quantityInput.val();
         var description = descriptionInput.val();
         
-        console.log("something submitted");
 
     });
 
+    
 
-});
+
+})();
