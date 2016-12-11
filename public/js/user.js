8976364ae5ab07b4cdafbc4a67222e469aee6de0
jonahmania <jmania999@gmail.com>
2016-12-10 21:51:16 -0500

    Add edit roles feature

--- public/js/user.js
+++ public/js/user.js
@@ -4,13 +4,19 @@
     var cancel = $("#cancelButton");
     var confirm = $("#confirmButton");
     var username = $("#username").text();
+    var rolesEditButton = $("#rolesEditButton");
+    var rolesInput = $("#rolesInput");
+    var rolesUpdateButton = $("#rolesUpdateButton");
+    var rolesField = $("#rolesField");
 
     deleteUser.click(function(){
         confirmDeleteWrapper.removeClass("hidden");
     });
+
     cancel.click(function(){
         confirmDeleteWrapper.addClass("hidden");
     });
+
     confirm.click(function(){
         //Make request to delete user
         var requestConfig = {
@@ -18,7 +24,7 @@
             url: "/users/delete/"+username,
             contentType: 'application/json'
         };
-        $.ajax(requestConfig).then(function (responseMessage){
+        $.ajax(requestConfig).then(function(responseMessage){
             //If we got a response populate the page
             if( responseMessage ){
                 window.location.href = "/users";
@@ -26,4 +32,26 @@
         });
     });
 
+    rolesEditButton.click(function(){
+        rolesInput.removeClass("hidden");
+        rolesUpdateButton.removeClass("hidden");
+    });
+
+    rolesUpdateButton.click(function(){
+        //Make request to update user
+        var requestConfig = {
+            method: "Put",
+            url: "/users/update/"+username,
+            data: JSON.stringify({
+                "roles":rolesInput.val()
+            }),
+            contentType: 'application/json'
+        };
+        $.ajax(requestConfig).then(function (responseMessage){
+            //If we got a response populate the page
+            if( responseMessage ){
+                rolesField.text(responseMessage.roles);
+            }
+        });
+    });
 })();
